ebabc61bf442c3d81336225edad0d01300d44996
nassir <daniya_n@yahoo.com>
2021-03-22 16:13:20 -0700

    acl,basic,bearer middleware rewrite

---
+++ src/auth/middleware/bearer.js
@@ -0,0 +1,19 @@
+
+const User = require('../models/users.js');
+module.exports = async(req,res,next)=>{
+
+  try{
+    if(!req.headers.authorization) {_autjError()}
+    const token = req.headers.authorization.split(' ').pop();
+    const validUser = await User.authenticateWithToken(token)
+
+   req.user = validUser;
+   req.token = validUser.token;
+   next()
+  }catch(e){
+    _authError();
+  }
+  function _authError(){
+    next('invalid Login')
+  }
+}
